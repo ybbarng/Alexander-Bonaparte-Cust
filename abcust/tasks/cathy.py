e1a6178a9882a3947e217010fa9789e2022aa96a
ybbarng <ybbarng@gmail.com>
2018-10-30 14:33:18 +0900

    최근 알림 메시지 가져오는 기능 구현

--- abcust/tasks/cathy.py
+++ abcust/tasks/cathy.py
@@ -24,8 +24,12 @@ def get_awair():
     return Awair(email=EMAIL, password=PASSWORD, access_token=ACCESS_TOKEN)
 
 
-def send_to_slack(color, message, fields=None, timestamp=None):
-    slack.write.delay('Cathy', color, None, message, fields, timestamp)
+def send_to_slack(color, title=None, message=None, fields=None, timestamp=None):
+    slack.write.delay('Cathy', color, title, message, fields, timestamp)
+
+
+def send_to_slack_blocking(color, title=None, message=None, fields=None, timestamp=None):
+    slack.write('Cathy', color, title, message, fields, timestamp)
 
 
 def notify_score(score):
@@ -54,7 +58,7 @@ def notify_score(score):
         color_index = 4
     elif score.score <= 90:
         color_index = 2
-    send_to_slack(COLORS[color_index], message, fields, score.timestamp.timestamp() + (9 * 3600)) # from utc to +09:00
+    send_to_slack(COLORS[color_index], None, message, fields, score.timestamp.timestamp() + (9 * 3600)) # from utc to +09:00
 
 
 @app.task
@@ -76,12 +80,40 @@ def get_timelines():
 
 
 @app.task
-def get_inbox_items():
-    start_dt = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    end_dt = start_dt + timedelta(days=1)
+def get_inbox_items(start_dt, end_dt):
     inbox_items = get_awair().get_inbox_items(start_dt, end_dt, 'ko')
-    for inbox_item in inbox_items:
-        print(inbox_item.timestamp)
-        print(inbox_item.title)
-        print(inbox_item.description)
     return inbox_items
+
+
+@app.task
+def get_inbox_items_batch(minutes=5):
+    end_dt = datetime.now()
+    start_dt = end_dt - timedelta(minutes=minutes)
+    inbox_items = get_inbox_items(start_dt, end_dt)
+    for inbox_item in reversed(inbox_items):
+        index = get_index(inbox_item)
+        fields = [{
+            'title': '경고 단계',
+            'value': '{} 단계'.format(index),
+            'short': False
+        }]
+        send_to_slack_blocking(COLORS[index],
+                      inbox_item.title,
+                      inbox_item.description,
+                      timestamp=inbox_item.timestamp.timestamp() + (9 * 3600)) # from utc to +09:00
+    return inbox_items
+
+
+def get_index(inbox_item):
+    try:
+        return int(inbox_item.icon_url[-5])
+    except ValueError:
+        fields = [{
+            'title': 'URL',
+            'value': inbox_item.icon_url,
+            'short': False
+        }]
+        send_to_slack(COLORS[4],
+                      message='알림 메시지의 경고 단계를 추정할 수 없습니다.',
+                      fields=fields)
+        return 4
