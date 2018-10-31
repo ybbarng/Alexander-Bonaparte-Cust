4adb508265e4a94a68b83baa9ebc36a0a8f02731
ybbarng <ybbarng@gmail.com>
2018-10-31 22:56:39 +0900

    슬랙에서 보낸 메시지가 변조되지 않았는지 체크

--- abcust/blueprints/slack.py
+++ abcust/blueprints/slack.py
@@ -1,6 +1,12 @@
 # A blueprint for slack actions
+from functools import wraps
+import hmac
+import hashlib
+import os
+from time import time
 
-from flask import abort, Blueprint, jsonify, request
+from dotenv import load_dotenv
+from flask import abort, Blueprint, jsonify, make_response, request
 
 from abcust.tasks import audrey
 from abcust.tasks import brice
@@ -8,10 +14,51 @@ from abcust.tasks import cathy
 from abcust.tasks import tts
 
 
+load_dotenv()
+signed_secret = os.getenv('SLACK_SIGNING_SECRET')
+
+
 slack = Blueprint('slack', __name__)
 
 
+def verify_slack_request(signed_secret):
+    def verify_slack_request_decorator(f):
+        def is_old(request):
+            # The request timestamp is more than five minutes from local time.
+            # It could be a replay attack, so let's ignore it.
+            request_timestamp = request.headers['X-Slack-Request-Timestamp']
+            return abs(time() - int(request_timestamp)) > 60 * 5
+
+        def verify_signature(request):
+            version = 'v0'
+            request_timestamp = request.headers['X-Slack-Request-Timestamp']
+            signature = request.headers['X-Slack-Signature']
+            body = request.get_data()
+            base_string = ':'.join([version, request_timestamp, body.decode('utf-8')])
+            calculated_hash = version + '=' + hmac.new(
+                signed_secret.encode('utf-8'),
+                base_string.encode('utf-8'),
+                hashlib.sha256
+            ).hexdigest()
+            # compare with compare_digest() to avoid timing attack
+            return hmac.compare_digest(calculated_hash, signature)
+
+        @wraps(f)
+        def function_wrapper(*args, **kwargs):
+            if is_old(request):
+                return make_response("", 403)
+
+            if not verify_signature(request):
+                return make_response("", 403)
+
+            return f(*args, **kwargs)
+
+        return function_wrapper
+    return verify_slack_request_decorator
+
+
 @slack.route('/slack', methods=['POST'])
+@verify_slack_request(signed_secret)
 def on_slack():
     command = request.form['command'][1:]
     action = None
